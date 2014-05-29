2372f6eacdd02a22afd23d032db6fc51c23f5c0a
Levi Brown <levi@coradine.com>
2014-05-29 11:18:59 -0600

    Updating test app to show UIView snapshot capabilities.
    Signed-off-by: Levi Brown <levi@coradine.com>

--- GRKBlurViewTestApp/GRKBlurViewTestApp/GRKViewController.m
+++ GRKBlurViewTestApp/GRKBlurViewTestApp/GRKViewController.m
@@ -9,18 +9,29 @@
 #import "GRKViewController.h"
 #import "GRKBlurView.h"
 
+typedef NS_ENUM(NSUInteger, GRKBlurTargetType) {
+    GRKBlurTargetTypeView = 0,
+    GRKBlurTargetTypeImage
+};
+
 @interface GRKViewController ()
 
 @property (nonatomic,weak) IBOutlet UIImageView *imageView;
 @property (nonatomic,weak) IBOutlet GRKBlurView *blurView;
 @property (nonatomic,weak) IBOutlet UISlider *blurSlider;
+@property (nonatomic,weak) IBOutlet UISlider *alphaSlider;
 @property (nonatomic,weak) IBOutlet UITextField *maxTextField;
 @property (nonatomic,weak) IBOutlet UILabel *currentRadiusLabel;
 @property (nonatomic,weak) IBOutlet UISlider *saturationSlider;
 @property (nonatomic,weak) IBOutlet UILabel *currentSaturationLabel;
+@property (nonatomic,weak) IBOutlet UISegmentedControl *segmentedControl;
+@property (nonatomic,weak) IBOutlet UIView *componentTestView;
+@property (nonatomic,weak) IBOutlet UIButton *snapshotButton;
 
 - (IBAction)blurSliderAction:(UISlider *)slider;
 - (IBAction)saturationSliderAction:(UISlider *)slider;
+- (IBAction)segmentedControlAction:(UISegmentedControl *)segmentedControl;
+- (IBAction)snapshotAction;
 
 @end
 
@@ -29,8 +40,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	
-    self.blurView.targetImage = self.imageView.image;
+
+    [self setBlurTarget:GRKBlurTargetTypeImage];
+
+    self.alphaSlider.value = self.blurView.alpha;
     [self updateBlurRadiusFromPercent:self.blurSlider.value];
 
     self.saturationSlider.value = self.blurView.saturationDeltaFactor;
@@ -44,6 +57,11 @@
     [self updateBlurRadiusFromPercent:slider.value];
 }
 
+- (IBAction)alphaSliderAction:(UISlider *)slider
+{
+    self.blurView.alpha = slider.value;
+}
+
 - (IBAction)saturationSliderAction:(UISlider *)slider
 {
     self.blurView.saturationDeltaFactor = slider.value;
@@ -51,7 +69,36 @@
     self.currentSaturationLabel.text = [NSString stringWithFormat:@"%.2f", self.blurView.saturationDeltaFactor];
 }
 
+- (IBAction)segmentedControlAction:(UISegmentedControl *)segmentedControl
+{
+    [self setBlurTarget:segmentedControl.selectedSegmentIndex];
+}
+
+- (IBAction)snapshotAction
+{
+    [self.blurView setTargetImageFromView:self.componentTestView];
+}
+
 #pragma mark - Helpers
+
+- (void)setBlurTarget:(GRKBlurTargetType)type
+{
+    self.segmentedControl.selectedSegmentIndex = type;
+    switch (type) {
+        case GRKBlurTargetTypeView:
+            [self.blurView setTargetImageFromView:self.componentTestView];
+            self.imageView.alpha = 0.0f;
+            self.snapshotButton.enabled = YES;
+            break;
+        case GRKBlurTargetTypeImage:
+            self.blurView.targetImage = self.imageView.image;
+            self.imageView.alpha = 1.0f;
+            self.snapshotButton.enabled = NO;
+            break;
+        default:
+            break;
+    }
+}
 
 - (void)updateBlurRadiusFromPercent:(CGFloat)percent
 {
